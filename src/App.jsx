1bc00a443a4bab7ff7e5b85a54bc0bdeccdb79c5
tomacho <tomacho28.th@gmail.com>
2024-09-23 09:15:43 -0300

    listo, terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,60 @@ import logo from "./assets/logosimpson.png";
 import Frases from "./componentes/Frases";
 import { Button } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { Commet } from "react-loading-indicators";
 
 function App() {
+  // guardar la api en un estado
 
-   // guardar la api en un estado 
-
-const [personaje, setPersonaje] = useState({})
+  const [personaje, setPersonaje] = useState({});
+  const [mostrarLoading, setmostrarLoading] = useState(true);
 
   useEffect(() => {
     consultarAPI();
   }, []);
 
-  const consultarAPI = async() => {
-    // fecha api
-    // solicitud GET(post, put, patch, delete) para traer datos y personajes de la API 
+  const consultarAPI = async () => {
+    try {
+      setmostrarLoading(true);
+      // fecha api
+      // solicitud GET(post, put, patch, delete) para traer datos y personajes de la API
 
-    const respuesta = await fetch ('https://thesimpsonsquoteapi.glitch.me/quotes');
-    const datos =  await respuesta.json()
-    console.log(datos[0]);
-    setPersonaje(datos[0]);
-    
-    
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
+      // verifico si me llego la respuesta
+       if( respuesta.status === 200 ) {
+        const datos = await respuesta.json();
+      console.log(datos[0]);
+      setPersonaje(datos[0]);
+
+      // mostrar el componente
+
+      setmostrarLoading(false);
+       } else 
+       {
+        // mostrar mensaje al usuario final 
+       }
+      
+    } catch (error) {
+      console.error(error);
+      // mostrar un mensaje al usuario final 
+       
+    }
   };
 
   return (
     <>
       <Container className="text-center my-4">
         <img src={logo} alt="" className="w-50" />
-        <Frases personaje={personaje}></Frases>
+        {mostrarLoading ? (
+          <div>
+            <Commet color="#32cd32" size="medium" text="" textColor="" />
+          </div>
+        ) : (
+          <Frases personaje={personaje}></Frases>
+        )}
+
         <Button variant="warning" className="mt-3 " onClick={consultarAPI}>
           Obtener Frase
         </Button>
